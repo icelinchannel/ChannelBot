a664fc321bed9f6e1f7289c82ded3923e9259b0c
icelinchannel <toimy4ka@gmail.com>
2023-11-26 11:34:01 +0300

    Create objects of Bot, Dispatcher and Router

--- bot/main.py
+++ bot/main.py
@@ -5,3 +5,15 @@ from aiogram.filters.chat_member_updated import JOIN_TRANSITION
 from aiogram.types import ChatMemberUpdated
 
 import asyncio
+
+from config import API_TOKEN
+
+
+bot = Bot(API_TOKEN)
+dp = Dispatcher()
+
+privat_rt = Router()
+community_rt = Router()
+
+privat_rt.message.filter(F.chat.type == 'privat')
+community_rt.message.filter(F.chat.type == 'group')
