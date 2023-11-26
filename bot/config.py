c40c30b7599d76eaceafcea6e254b0a78ea7ff0e
icelinchannel <toimy4ka@gmail.com>
2023-11-26 18:41:47 +0300

    Add new constants

--- bot/config.py
+++ bot/config.py
@@ -3,3 +3,6 @@ from os import getenv
 
 load_dotenv()
 API_TOKEN = getenv('TG_TOKEN')
+WELCOME_IMAGE_LINK = getenv('WELCOME_IMAGE_LINK') # image is in cloud storage, if you use this code, add your link here
+CHAT_ID = int(getenv("ICELIN_CHAT_ID"))
+CHANNEL_ID = int(getenv('ICELIN_CHANNEL_ID'))
