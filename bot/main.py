7473f38b6fa0ae4d96b14ca7fe9248b7b4695cad
icelinchannel <toimy4ka@gmail.com>
2023-11-26 11:42:01 +0300

    Add welcome function

--- bot/main.py
+++ bot/main.py
@@ -2,7 +2,6 @@ from aiogram import Bot, Dispatcher, F, Router, types
 
 from aiogram.filters import ChatMemberUpdatedFilter
 from aiogram.filters.chat_member_updated import JOIN_TRANSITION
-from aiogram.types import ChatMemberUpdated
 
 import asyncio
 
@@ -18,3 +17,20 @@ community_rt = Router()
 privat_rt.message.filter(F.chat.type == 'privat')
 community_rt.message.filter(F.chat.type == 'group')
 community_rt.chat_member.filter(F.chat.type == 'group')
+
+@community_rt.chat_member(ChatMemberUpdatedFilter(JOIN_TRANSITION))
+async def welcome(event: types.ChatMemberUpdated):
+    return event.reply_photo(photo='images/hello.jpg', caption='''Добро пожаловать в чат👋👋👋
+🔴 У нас тут правила чата, всё как везде:
+
+👉 Уважайте других, относитесь к ним так, как хотите, чтобы относились к вам
+
+👉 Никого не оскорбляйте, не нужно плодить агрессию. Давайте лучше распространять доброе общение)
+
+👉 Никакой рекламы в чате, понятно?😉
+
+👉 Не присылайте сюда шокирующий контент и порнографию
+
+👉 Мы оставляем за собой право удалять любые сообщения и пользователей из группы
+
+❗️ Занудство закончилось, можно писать)''')
