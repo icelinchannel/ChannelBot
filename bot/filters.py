fe4ee8a4b80f24d2d53bb1cacbeed1a77201ae30
icelinchannel <toimy4ka@gmail.com>
2023-11-30 11:41:23 +0300

    Add IsItBotFilter

--- bot/filters.py
+++ bot/filters.py
@@ -3,4 +3,15 @@ from aiogram.filters.base import Filter
 from aiogram.enums.chat_type import ChatType
 from aiogram import F
 
-from config import CHANNEL_ID, GROUP_ID
+from config import CHANNEL_ID, GROUP_ID, bot
+
+
+class IsItBotFilter(Filter):
+    async def __call__(self, event):
+        bot_obj = await bot.get_me()
+        bot_id = bot_obj.id
+
+        for chat_member in event.new_chat_members:
+            if chat_member.id == bot_id:
+                return True
+        return F.chat.type == ChatType.SUPERGROUP
