9f45ed1ba5950df0700e376f6d715feba68dafcb
icelinchannel <toimy4ka@gmail.com>
2023-12-02 14:26:33 +0300

    Update

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,10 +1,13 @@
-new_env_file = open('.env', 'w')
+new_env_file = open('../.env', 'w')
 
-text = '''TG_TOKEN = "<your Telegram bot token here>"
+with open('../.python-version', 'r') as python_version_file:
+    python_version = python_version_file.read()
+
+text = f'''TG_TOKEN = "<your Telegram bot token here>"
 WELCOME_IMAGE_LINK = "<a link to your image>"
 ICELIN_GROUP_ID = "<your Telegram comments group id>"
 ICELIN_CHANNEL_ID = "<your Telegram channel id here>"
-'''
+PYTHON_VERSION = {python_version}'''
 
 new_env_file.write(text)
 new_env_file.close()
