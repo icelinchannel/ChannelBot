b71f99976be5011a0d189ce6e0a4fcf05f50aa54
Icelin <toimy4ka@gmail.com>
2024-01-03 18:38:15 +0300

    Fix

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,6 @@
 new_env_file = open('bot/.env', 'w')
 
-text = f'''TG_TOKEN = "<your Telegram bot token here>"
+text = '''TG_TOKEN = "<your Telegram bot token here>"
 WELCOME_IMAGE_LINK = "<a link to your image>"
 ICELIN_GROUP_ID = "<your Telegram comments group id>"
 ICELIN_CHANNEL_ID = "<your Telegram channel id here>"
