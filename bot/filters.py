9efad6390e46511933b69f7b3cc61171fb4767ce
Icelin <toimy4ka@gmail.com>
2024-01-05 15:44:15 +0300

    Update

--- bot/filters.py
+++ bot/filters.py
@@ -24,10 +24,14 @@ logger.addHandler(stdout_handler)
 
 
 class ChatTypeFilter(Filter):
-    def __init__(self, chat_type: Union[str, list]):
+    def __init__(self, chat_type: Union[str, list]) -> None:
         self.chat_type = chat_type
+        logger.info('ChatTypeFilter was initialized')
 
     async def __call__(self, message: types.Message) -> bool:
+
+        logger.info('ChatTypeFilter was used')
+
         if isinstance(self.chat_type, str):
             return message.chat.type == self.chat_type
         else:
@@ -35,26 +39,30 @@ class ChatTypeFilter(Filter):
 
 
 class PrivateRouterFilter(Filter):
-    async def __call__(self, message):
-        return ChatTypeFilter(chat_type=[ChatType.PRIVATE]) and message.chat.id != OWNER_ID
+    async def __call__(self, message) -> bool:
+        logger.info('PrivateRouterFilter was used')
+        return ChatTypeFilter(chat_type=[ChatType.PRIVATE]).__call__(message=message) and message.chat.id != OWNER_ID
 
 
 class GroupRouterFilter(Filter):
-    async def __call__(self, message):
+    async def __call__(self, message) -> bool:
+        logger.info('GroupRouterFilter was used')
         return message.chat.id == GROUP_ID
 
 
 class ChannelRouterFilter(Filter):
-    async def __call__(self, message):
+    async def __call__(self, message) -> bool:
+        logger.info('ChannelRouterFilter was used')
         return message.chat.id == CHANNEL_ID
 
 
 class OwnerRouterFilter(Filter):
-    async def __call__(self, message):
+    async def __call__(self, message) -> bool:
+        logger.info('OwnerRouterFilter was used')
         return message.chat.id == OWNER_ID
 
 
 class IsItThisBotFilter(Filter):
-    async def __call__(self, event: types.ChatMemberUpdated):
+    async def __call__(self, event: types.ChatMemberUpdated) -> bool:
         logger.info('IsItThisBotFilter was used')
         return event.new_chat_member.user.id == bot.id
