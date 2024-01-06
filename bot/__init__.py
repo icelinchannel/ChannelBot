820e5ffb78e3658685a0bb34561c706b944d1269
Icelin <toimy4ka@gmail.com>
2024-01-06 19:28:33 +0300

    Add enviroment variable

--- bot/__init__.py
+++ bot/__init__.py
@@ -4,6 +4,7 @@ text = '''TG_TOKEN = "<your Telegram bot token here>"
 WELCOME_IMAGE_LINK = "<a link to your image>"
 ICELIN_GROUP_ID = "<your Telegram comments group id>"
 ICELIN_CHANNEL_ID = "<your Telegram channel id here>"
+OWNER_TG_ID = "<Telegram ID of a person, who has to anonimously contact with bot's users>"
 PYTHON_VERSION = 3.11.3'''
 
 new_env_file.write(text)
